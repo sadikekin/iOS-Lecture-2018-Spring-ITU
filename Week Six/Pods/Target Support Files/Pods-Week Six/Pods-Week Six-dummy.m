ad6e875f2116329794ecef66a5f0bec5bdbdfbcb
Sadik Ekin Ozbay <ozbaysa@itu.edu.tr>
2018-04-16 19:35:48 +0300

    week6

---
+++ Week Six/Pods/Target Support Files/Pods-Week Six/Pods-Week Six-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Week_Six : NSObject
+@end
+@implementation PodsDummy_Pods_Week_Six
+@end
--- Week Six/Week Six/MainTwoVC.swift
+++ Week Six/Week Six/MainTwoVC.swift
@@ -7,29 +7,36 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import NVActivityIndicatorView
 
-class MainTwoVC: UIViewController {
-
+class MainTwoVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let tableView = UITableView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
+    
+    let loader = NVActivityIndicatorView(frame: CGRect(x: 100, y: 300, width: 64, height: 64), type: .pacman, color: .red, padding: 10)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.view.addSubview(loader)
+        self.view.addSubview(tableView)
+        fetchData()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func fetchData() {
+        let header = [
+            "accept": "application/json",
+            "apikey": ""
+        ]
+        loader.startAnimating()
+        Alamofire.request("https://api.hurriyet.com.tr/v1/articles", headers: header).responseJSON { response in
+            if let json = response.result.value {
+                let jsonData = JSON(json)
+                //self.label.text = jsonData[0]["Title"].string
+                self.loader.stopAnimating()
+            }
+        }
     }
-    */
 
 }
